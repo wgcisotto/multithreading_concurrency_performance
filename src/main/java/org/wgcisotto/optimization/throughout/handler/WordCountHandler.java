f35ee42cd367bd3be031148d5bcd4dd90bf7a292
William Cisotto <willgalindo@gmail.com>
2022-07-23 22:28:03 +0100

    throughout multithreading

---
+++ src/main/java/org/wgcisotto/optimization/throughout/handler/WordCountHandler.java
@@ -0,0 +1,46 @@
+package org.wgcisotto.optimization.throughout.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WordCountHandler implements HttpHandler {
+        private String text;
+
+        public WordCountHandler(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException, IOException {
+            String query = httpExchange.getRequestURI().getQuery();
+            String[] keyValue = query.split("=");
+            String action = keyValue[0];
+            String word = keyValue[1];
+            if (!action.equals("word")) {
+                httpExchange.sendResponseHeaders(400, 0);
+                return;
+            }
+            long count = countWord(word);
+            byte[] response = Long.toString(count).getBytes();
+            httpExchange.sendResponseHeaders(200, response.length);
+            OutputStream outputStream = httpExchange.getResponseBody();
+            outputStream.write(response);
+            outputStream.close();
+        }
+
+        private long countWord(String word) {
+            long count = 0;
+            int index = 0;
+            while (index >= 0) {
+                index = text.indexOf(word, index);
+                if (index >= 0) {
+                    count++;
+                    index++;
+                }
+            }
+            return count;
+        }
+    }
